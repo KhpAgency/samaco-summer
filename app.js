42e3a9f07f94a16b00dee731458c848134dde0d8
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-04-30 02:20:37 +0300

    server ping update to 14 mins

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const server = app.listen(PORT, () => {
 pingServer();
 
 // Ping the server every 12 minutes (12 * 60 * 1000 milliseconds)
-const pingInterval = 12 * 60 * 1000;
+const pingInterval = 14 * 60 * 1000;
 setInterval(pingServer, pingInterval);
 
 // Function to ping the server by hitting the specified API route
