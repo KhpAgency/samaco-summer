14d70536844c96a7fc2e7a21260251ab707cae8b
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2024-04-29 11:22:47 +0300

    server ping

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config.env" });
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 const morgan = require("morgan");
 const cors = require("cors");
+const https = require('https'); // Change from 'http' to 'https'
 
 const ApiError = require("./utils/ApiError");
 const globalError = require("./middlewares/errorMiddleware");
@@ -14,8 +15,7 @@ const authRoute = require("./routes/authRoute");
 const usersRoute = require("./routes/usersRoute");
 const formsRoute = require("./routes/formRoute");
 
-// middlewares
-
+// Middlewares
 app.use(cors());
 app.options("*", cors());
 app.use(express.urlencoded({ extended: false }));
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === "development") {
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-// DB connecetion
+// DB connection
 dbConnection();
 
 // Mount Routes
@@ -35,20 +35,41 @@ app.use("/api/v1/users", usersRoute);
 app.use("/api/v1/forms", formsRoute);
 
 app.all("*", (req, res, next) => {
-  next(new ApiError(`can't find this route: ${req.originalUrl}`, 400));
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
 });
+
 // Global error handling middleware
 app.use(globalError);
 
-const server = app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`)
-);
+// Start the server
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+// Ping the server immediately after starting the server
+pingServer();
+
+// Ping the server every 12 minutes (12 * 60 * 1000 milliseconds)
+const pingInterval = 12 * 60 * 1000;
+setInterval(pingServer, pingInterval);
+
+// Function to ping the server by hitting the specified API route
+function pingServer() {
+  const pingEndpoint = 'https://samaco-summer.onrender.com/api/v1/auth/login';
+
+  // Send a GET request to the ping endpoint
+  https.get(pingEndpoint, (res) => {
+    console.log(`Ping sent to server: ${res.statusCode}`);
+  }).on('error', (err) => {
+    console.error('Error while sending ping:', err);
+  });
+}
 
 // UnhandledRejections event handler (rejection outside express)
 process.on("unhandledRejection", (err) => {
-  console.error(`unhandledRejection Errors: ${err.name} | ${err.message}`);
+  console.error(`Unhandled Rejection Error: ${err.name} | ${err.message}`);
   server.close(() => {
-    console.log("server shutting down...");
+    console.log("Server shutting down...");
     process.exit(1);
   });
 });
